cb6ed2d1b4f0274a1efdd7aa0895a061328952e4
Raphaël Fromentin <rafbodaha@gmail.com>
2025-11-10 14:09:08 +0100

    Init

---
+++ main.py
@@ -0,0 +1,30 @@
+from random import randint
+
+def ask_int() -> None:
+  guess = None
+  while guess is None:
+    try:
+      value = int(input("Entrez un nombre entre 1 et 10: "))
+      if value >= 1 and value <= 10:
+        guess = value
+      else:
+        print(value, "n'est pas entre 1 et 10")
+    except ValueError:
+      print("Entier invalide. Réessayez.")
+  return guess
+
+if __name__ == "__main__":
+  value = randint(1, 11)
+  guess = -1
+  n = 0
+  while guess != value:
+    guess = ask_int()
+    if guess == value:
+      print("Gagné")
+    elif guess > value:
+      print("Le nombre à trouver est plus petit")
+    else:
+      print("Le nombre à trouver est plus petit")
+    n += 1
+
+  print("Trouvé en", n, "essais")
